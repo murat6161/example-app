5febec890abed681e9ff9dffbf49fa15279a77a5
Murat <mrtgms3561@gmail.com>
2021-03-02 22:50:36 +0200

    Updates

---
+++ static/js/main.e2fb087c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpexample-app"]=this["webpackJsonpexample-app"]||[]).push([[0],{4:function(e,t,c){"use strict";c.r(t);c(1);var n=c(3),r=c.n(n),p=c(0),s=function(){return Object(p.jsx)("div",{children:Object(p.jsx)("h1",{children:"Hi React"})})};r.a.render(Object(p.jsx)(s,{}),document.querySelector("#root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.e2fb087c.chunk.js.map
